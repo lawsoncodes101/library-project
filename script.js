557658330c7819cd86c64915db203df487dad794
lawson101 <lawsoncodes101@gmail.com>
2026-01-05 15:18:00 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-// TODO: Constructor
-// Initialize the Book constructor
-// Collects 4 args - id, title, author, pages
-// Set isRead to false by default
-// Use Book.prototype to create toggleFunction();
+const dialog = document.querySelector("dialog");
+const openDialog = document.querySelector(".openDialog");
+const addLibraryBtn = document.querySelector("button[type='submit']");
+const library = document.querySelector(".library");
+const myLibrary = [];
+loadFromLocalStorage();
 
-// TODO: myLibrary
-// Initialize myLibrary as localStorage.getItem("myLibrary") || [];
+function loadFromLocalStorage() {
+    const stored = JSON.parse(localStorage.getItem("myLibrary")) || [];
+    if (stored.length < 1) return false;
 
-// TODO: updateLocalStorage();
-// Update local storage using localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-
-// TODO: addBookToLibrary();
-// Collects 3 args - title, author, pages
-// Create dataId with crypto.randomUUID();
-// Create the new book with Book(dataId, title, author, pages);
-// Run displayInHTML(); with 4 args - dataId, title, author, pages
-// Push newBook to myLibrary
-// Use updateLocalStorage();
-
-// TODO: displayInHTML();
-// Collects 4 args - args - dataId, title, author, pages
-// Create parent div
-// Create spans for title, author, pages and set textContent
-// Create data-id attribute for the div as the data-id arg;
-// Create Remove and Read buttons and set event listeners
-// Remove Event Listener - Run removeFromLibrary();
-// Read Event Listener - Find index of book with that id and run myLibrary[index].toggleRead();
-// Append all elements to div
-// Append div to library div
-
-// TODO: removeFromLibrary();
-// Collect 1 arg - id
-// Find the index of book with that id in myLibrary and splice the index
-// Run updateLocalStorage();
-
-const library = document.querySelector("#library");
-const myLibrary = JSON.parse(localStorage.getItem("myLibrary") || []);
+    // Recreate Protoype Chain
+    myLibrary.length = 0;
+    for (const obj of stored) {
+        const book = new Book(obj.id, obj.title, obj.author, obj.pages);
+        book.hasRead = !!obj.hasRead; // Ensures Boolean type conversion
+        myLibrary.push(book);
+        displayInHTML(book.id, book.title, book.author, book.pages);
+    }
+}
 
 function Book(id, title, author, pages) {
     if (!new.target)
@@ -66,14 +48,16 @@ function addBookToLibrary(title, author, pages) {
 }
 
 function displayInHTML(dataId, title, author, pages) {
-    const div = document.createElement("div");
+    const parentDiv = document.createElement("div");
     const titleDisplay = document.createElement("span");
     const authorDisplay = document.createElement("span");
     const pagesDisplay = document.createElement("span");
     const readBtn = document.createElement("button");
     const removeBtn = document.createElement("button");
+    const buttonContainer = document.createElement("div");
 
-    div.setAttribute("data-id", dataId);
+    parentDiv.classList.add("book");
+    parentDiv.setAttribute("data-id", dataId);
     titleDisplay.textContent = title;
     authorDisplay.textContent = author;
     pagesDisplay.textContent = pages;
@@ -81,19 +65,41 @@ function displayInHTML(dataId, title, author, pages) {
     removeBtn.textContent = "Remove";
 
     readBtn.addEventListener("click", function () {
-        let index = myLibrary.indexOf((x) => x.id === dataId);
+        const index = myLibrary.findIndex(x => x.id === dataId);
+        if (index === -1) return;
         myLibrary[index].toggleRead();
+        updateLocalStorage();
+        console.log(myLibrary[index].hasRead);
     });
     removeBtn.addEventListener("click", function () {
-        removeFromLibrary(dataId);
+        removeFromLibrary(dataId, removeBtn.parentElement);
     });
 
-    div.append(titleDisplay, authorDisplay, pagesDisplay, readBtn, removeBtn);
-    library.append(div);
+    buttonContainer.append(readBtn, removeBtn);
+    parentDiv.append(titleDisplay, authorDisplay, pagesDisplay, buttonContainer);
+    library.append(parentDiv);
 }
 
-function removeFromLibrary(dataId) {
-    let index = myLibrary.indexOf((x) => x.id === dataId);
-    myLibrary.splice(index, 1);
+function removeFromLibrary(dataId, element) {
+    let parentElement = element.parentElement;
+    parentElement.removeChild(element);
+
+    const index = myLibrary.findIndex(x => x.id === dataId);
+    if (index !== -1) myLibrary.splice(index, 1);
     updateLocalStorage();
 }
+
+addLibraryBtn.addEventListener("click", function () {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = Number(document.querySelector("#pages").value);
+
+    if (pages < 1 && pages > 1000) return false;
+
+    addBookToLibrary(title, author, pages);
+    dialog.close();
+});
+
+openDialog.addEventListener("click", function () {
+    dialog.show();
+});
